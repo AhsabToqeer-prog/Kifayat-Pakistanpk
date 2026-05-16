8fbce9f93414d8f47092558cb76dd45822a95c87
AhsabToqeer-prog <toqeerwattoo@icloud.com>
2026-05-16 13:39:10 +0500

    Update campaigns.ts

--- lib/db/src/schema/campaigns.ts
+++ lib/db/src/schema/campaigns.ts
@@ -1,6 +1,6 @@
 import { pgTable, text, serial, integer, date } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod/v4";
+import { z } from "zod";
 
 export const campaignsTable = pgTable("campaigns", {
   id: serial("id").primaryKey(),
